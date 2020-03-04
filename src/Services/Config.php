51eb2fd6cd7078e5bbab75f56bf0ac7eb0891de0
GeekQu <iloves@live.com>
2020-03-04 23:30:25 +0800

    Update Config.php

--- src/Services/Config.php
+++ src/Services/Config.php
@@ -14,7 +14,7 @@ class Config
 
     public static function getconfig($key)
     {
-        $value = GConfig::where('key', '=', $key);
+        $value = GConfig::where('key', '=', $key)->first();
         if ($value === null) {
             $value = DefaultConfig::firstOrCreate($key);
         }
